8d410cbec04f9d77a6026f2ecb8d16a208128913
Nils Quirmbach <nils.quirmach@outlook.com>
2025-11-11 22:42:18 +0100

    update tf az module to latest

--- terraform/modules/services/main.tf
+++ terraform/modules/services/main.tf
@@ -92,7 +92,7 @@ resource "azurerm_storage_account" "artifacts" {
 # Create artifacts container
 resource "azurerm_storage_container" "artifacts" {
   name                  = "artifacts"
-  storage_account_id    = azurerm_storage_account.artifacts.id
+  storage_account_name  = azurerm_storage_account.artifacts.name
   container_access_type = "private"
 }
 
